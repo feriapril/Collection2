d70a4eed26c8fb85a2ae683da7883c54c662dffa
feriapril <61922621+feriapril@users.noreply.github.com>
2020-06-29 13:36:35 +0700

    Add files via upload

---
+++ ClassInduk/Karyawan.cs
@@ -0,0 +1,9 @@
+﻿namespace Collection.induk
+{
+    public abstract class Karyawan
+    {
+        public abstract string Nama { get; set; }
+        public abstract string Nik { get; set; }
+        public abstract double TotalGaji();
+    }
+}
